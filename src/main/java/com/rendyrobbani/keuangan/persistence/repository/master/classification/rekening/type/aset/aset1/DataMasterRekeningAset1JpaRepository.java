59f7e9d416a0bb8d151fd730da6b61e6769cfb0f
Rendy Robbani <rendyrobbani17@gmail.com>
2025-08-20 16:35:54 +0800

    Development

---
+++ src/main/java/com/rendyrobbani/keuangan/persistence/repository/master/classification/rekening/type/aset/aset1/DataMasterRekeningAset1JpaRepository.java
@@ -0,0 +1,9 @@
+package com.rendyrobbani.keuangan.persistence.repository.master.classification.rekening.type.aset.aset1;
+
+import com.rendyrobbani.keuangan.core.domain.entity.master.classification.rekening.type.aset.aset1.DataMasterRekeningAset1;
+import com.rendyrobbani.keuangan.persistence.entity.master.classification.rekening.type.aset.aset1.DataMasterRekeningAset1Entity;
+import com.rendyrobbani.keuangan.persistence.repository.master.classification.rekening.base.rekening1.DataMasterRekening1JpaRepository;
+
+public interface DataMasterRekeningAset1JpaRepository extends DataMasterRekening1JpaRepository<DataMasterRekeningAset1Entity, DataMasterRekeningAset1> {
+
+}
---
+++ src/main/java/com/rendyrobbani/keuangan/persistence/repository/master/classification/rekening/type/aset/aset1/SipdMasterRekeningAset1JpaRepository.java
@@ -0,0 +1,10 @@
+package com.rendyrobbani.keuangan.persistence.repository.master.classification.rekening.type.aset.aset1;
+
+import com.rendyrobbani.keuangan.core.domain.entity.master.classification.rekening.type.aset.aset1.DataMasterRekeningAset1;
+import com.rendyrobbani.keuangan.core.domain.entity.master.classification.rekening.type.aset.aset1.SipdMasterRekeningAset1;
+import com.rendyrobbani.keuangan.persistence.entity.master.classification.rekening.type.aset.aset1.SipdMasterRekeningAset1Entity;
+import com.rendyrobbani.keuangan.persistence.repository.master.classification.rekening.base.rekening1.SipdMasterRekening1JpaRepository;
+
+public interface SipdMasterRekeningAset1JpaRepository extends SipdMasterRekening1JpaRepository<SipdMasterRekeningAset1Entity, SipdMasterRekeningAset1, DataMasterRekeningAset1> {
+
+}
