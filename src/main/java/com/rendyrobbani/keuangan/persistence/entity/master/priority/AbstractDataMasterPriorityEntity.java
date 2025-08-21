e6a7e32f36a9de49a9ddbec49d5b9467da883068
Rendy Robbani <rendyrobbani17@gmail.com>
2025-08-22 07:27:18 +0800

    Development

---
+++ src/main/java/com/rendyrobbani/keuangan/persistence/entity/master/priority/AbstractDataMasterPriorityEntity.java
@@ -0,0 +1,77 @@
+package com.rendyrobbani.keuangan.persistence.entity.master.priority;
+
+import com.rendyrobbani.keuangan.core.domain.entity.master.priority.DataMasterPriorityEntity;
+import com.rendyrobbani.keuangan.persistence.entity.master.AbstractDataMasterEntity;
+import com.rendyrobbani.keuangan.persistence.entity.user.DataUserEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import java.time.LocalDateTime;
+
+@Data
+@Setter(AccessLevel.NONE)
+@Accessors(fluent = true)
+@EqualsAndHashCode(callSuper = true)
+@MappedSuperclass
+public abstract class AbstractDataMasterPriorityEntity<DOMAIN extends DataMasterPriorityEntity> extends AbstractDataMasterEntity<DOMAIN, Long> implements DataMasterPriorityEntity {
+
+	@Id
+	@Column(name = "id", nullable = false, updatable = false)
+	protected Long id;
+
+	@Column(name = "year", nullable = false)
+	protected Short year;
+
+	@Column(name = "number", nullable = false)
+	protected Byte number;
+
+	@Column(name = "name", nullable = false)
+	protected String name;
+
+	@Column(name = "sipdName", nullable = false)
+	protected String sipdName;
+
+	@Column(name = "is_locked", nullable = false)
+	protected boolean isLocked;
+
+	@Column(name = "locked_at")
+	protected LocalDateTime lockedAt;
+
+	@Column(name = "locked_by", length = 18)
+	protected String lockedBy;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumns(value = {
+			@JoinColumn(name = "locked_by", referencedColumnName = "id", insertable = false, updatable = false)
+	})
+	protected DataUserEntity lockedByAsDataUser;
+
+	@Override
+	public void lock(LocalDateTime lockedAt, String lockedBy) {
+		this.isLocked = true;
+		this.lockedAt = lockedAt;
+		this.lockedBy = lockedBy;
+	}
+
+	@Override
+	public void unlock(LocalDateTime updatedAt, String updatedBy) {
+		this.isLocked = false;
+		this.lockedAt = null;
+		this.lockedBy = null;
+		this.updatedAt = updatedAt;
+		this.updatedBy = updatedBy;
+	}
+
+	@Override
+	public void delete(LocalDateTime deletedAt, String deletedBy) {
+		lock(deletedAt, deletedBy);
+		super.delete(deletedAt, deletedBy);
+	}
+
+
+
+}
