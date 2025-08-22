79b77159022e33549afe40b59b69d286b4849fc7
Rendy Robbani <rendyrobbani17@gmail.com>
2025-08-22 21:46:23 +0800

    Development

--- src/main/resources/sql/migration/007-data_master_kecamatan.sql
+++ src/main/resources/sql/migration/007-data_master_kecamatan.sql
@@ -1,7 +1,7 @@
 set @action_at = now();
 set @action_by = '199510172019031003';
 
-insert into keuangan_dev.data_master_kabupaten (id, code, name, is_locked, locked_at, locked_by, created_at, created_by, updated_at, updated_by, is_deleted, deleted_at, deleted_by, provinsi_id)
+insert into keuangan_dev.data_master_kecamatan (id, code, name, is_locked, locked_at, locked_by, created_at, created_by, updated_at, updated_by, is_deleted, deleted_at, deleted_by, provinsi_id, kabupaten_id)
 select *
 from (
     select kode                                                   as id
@@ -18,7 +18,8 @@ from (
          , if(is_deleted is null or is_deleted, @action_at, null) as deleted_at
          , if(is_deleted is null or is_deleted, @action_by, null) as deleted_by
          , left(kode, 2)                                          as provinsi_id
-    from espresso_2025_preproduction.data_master_kabupaten
+         , left(kode, 5)                                          as kabupaten_id
+    from espresso_2025_preproduction.data_master_kecamatan
 ) t
 on duplicate key update code       = t.code
                       , name       = t.name
